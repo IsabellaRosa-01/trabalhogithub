44fea1f416578ebf2e4ee6f603ed90f83ab024fe
IsabellaRosa-01 <isabellasilva.is10@gmail.com>
2026-04-19 21:59:32 -0300

    login usuário

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 import json
 from datetime import datetime
+def login(self):
+    print("\n=== LOGIN ===")
+    user = input("Usuário: ")
+    senha = input("Senha: ")
+
+    if user in self.usuarios and self.usuarios[user] == senha:
+        print("✔ Login realizado com sucesso!")
+        return True
+    else:
+        print("❌ Usuário ou senha inválidos!")
+        return False
