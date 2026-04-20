10d75a736e9df9f9b9f1bcbace65ee95c004a62b
IsabellaRosa-01 <isabellasilva.is10@gmail.com>
2026-04-19 22:08:23 -0300

    menu sistema

--- main.py
+++ main.py
@@ -1,2 +1,44 @@
 import json
 from datetime import datetime
+def menu():
+    sistema = SistemaFarmacia()
+
+    if not sistema.login():
+        return
+
+    while True:
+        print("\n=== MENU ===")
+        print("1 - Cadastrar produto")
+        print("2 - Entrada de estoque")
+        print("3 - Vender produto")
+        print("4 - Listar produtos")
+        print("5 - Verificar vencimentos")
+        print("6 - Estoque baixo")
+        print("7 - Relatório de vendas")
+        print("0 - Sair")
+
+        op = input("Escolha: ")
+
+        if op == "1":
+            sistema.cadastrar_produto()
+        elif op == "2":
+            sistema.entrada()
+        elif op == "3":
+            sistema.vender()
+        elif op == "4":
+            sistema.listar_produtos()
+        elif op == "5":
+            sistema.verificar_vencimento()
+        elif op == "6":
+            sistema.estoque_baixo()
+        elif op == "7":
+            sistema.relatorio_vendas()
+        elif op == "0":
+            print("Encerrando...")
+            break
+        else:
+            print("Opção inválida!")
+
+
+if __name__ == "__main__":
+    menu()
